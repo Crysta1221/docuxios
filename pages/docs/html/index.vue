2b657e8e0b5d198aa5462aea610ec7d339a1cfbd
Crysta1221 <takuyakojim@outlook.jp>
2023-12-22 02:21:53 +0900

    :art: Change layout

--- pages/docs/html/index.vue
+++ pages/docs/html/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <Navbar/>
-    <div class="ml-6 mt-4 mr-6">
-      <ContentDoc class="markdown-body" path="/html" data-theme="light"/>
+    <div class="mt-4 flex justify-center">
+      <ContentDoc class="markdown-body" path="/html"/>
     </div>   
   </main>
 </template>
